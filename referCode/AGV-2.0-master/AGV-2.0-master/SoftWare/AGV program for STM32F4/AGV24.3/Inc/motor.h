82d734b539d5f229918f4a0bd6e0eb15b6a8aa2f
Trieu Huynh <vikingtc4@gmail.com>
2022-11-05 01:16:43 +0700

    Deleting folder

--- referCode/AGV-2.0-master/AGV-2.0-master/SoftWare/AGV program for STM32F4/AGV24.3/Inc/motor.h
+++
@@ -1,9 +0,0 @@
-//-----------------------------------------------------BKHN------------------------------------------------//
-//---------------------------------------------------AGV_Ver_2---------------------------------------------//
-//---------------------------------------------------Lam_Tran----------------------------------------------//
-#include "stm32f4xx_hal.h"
-extern TIM_HandleTypeDef htim3;
-void motorLeftForward(uint32_t speed);
-void motorLeftBackward(uint32_t speed);
-void motorRightForward(uint32_t speed);
-void motorRightBackward(uint32_t speed);
