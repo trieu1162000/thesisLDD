6bbebf3b7e3aa907425bd78bc6bb7115a25a03d1
Trieu Huynh Pham Nhat <86428744+trieu1162000@users.noreply.github.com>
2022-11-19 17:56:55 +0700

    Update setup.sh

--- kernelModule/008_pwm_motor/setup.sh
+++ kernelModule/008_pwm_motor/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-dtc pwm_rasp.dts -O dtb > pwm_rasp.dtbo
-sudo cp pwm_rasp.dtbo /boot/overlays/
+dtc pwm_motor_driver_rasp.dts -O dtb > pwm_motor_driver_rasp.dtbo
+sudo cp pwm_motor_driver_rasp.dtbo /boot/overlays/
